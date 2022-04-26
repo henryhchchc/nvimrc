36a90d3f5349328a30bc40f579269eaea0a34497
Henry Chu <henrytech@outlook.com>
2022-04-26 16:05:18 +0800

    Updated shortcuts

--- lua/config/components/ui/navigator.lua
+++ lua/config/components/ui/navigator.lua
@@ -3,10 +3,10 @@ function M.configure()
     local navigator = require("Navigator")
     navigator.setup()
 
-    vim.keymap.set("n", "<C-w><C-h>", navigator.left, { desc = "Navigator Go Left" })
-    vim.keymap.set("n", "<C-w><C-j>", navigator.down, { desc = "Navigator Go Down" })
-    vim.keymap.set("n", "<C-w><C-k>", navigator.up, { desc = "Navigator Go Up" })
-    vim.keymap.set("n", "<C-w><C-l>", navigator.right, { desc = "Navigator Go Right" })
+    vim.keymap.set("n", "<C-h>", navigator.left, { desc = "Navigator Go Left" })
+    vim.keymap.set("n", "<C-j>", navigator.down, { desc = "Navigator Go Down" })
+    vim.keymap.set("n", "<C-k>", navigator.up, { desc = "Navigator Go Up" })
+    vim.keymap.set("n", "<C-l>", navigator.right, { desc = "Navigator Go Right" })
     vim.keymap.set("n", "<C-w><C-p>", navigator.previous, { desc = "Navigator Go Back" })
 end
 
