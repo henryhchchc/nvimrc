9de1518c4038b63130409cb641a67a0f72954b8b
Henry Chu <henrytech@outlook.com>
2022-05-05 23:01:05 +0800

    ignored ds store for nvimtree

--- lua/config/components/ui/nvimtree.lua
+++ lua/config/components/ui/nvimtree.lua
@@ -50,7 +50,7 @@ function M.configure()
         diagnostics = { enable = true },
         update_focused_file = { enable = true },
         system_open = { cmd = "open" },
-        filters = { custom = { "^\\.git$" } },
+        filters = { custom = { "^\\.git$", "^\\.DS_Store$" } },
         git = { ignore = false },
         view = {
             width = 30,
