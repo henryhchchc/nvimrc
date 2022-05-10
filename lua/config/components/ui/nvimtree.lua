bd5c874e02ed8dce7dada692824bf95fae2621b2
Henry Chu <henrytech@outlook.com>
2022-05-10 20:26:40 +0800

    Updated nvimtree settings

--- lua/config/components/ui/nvimtree.lua
+++ lua/config/components/ui/nvimtree.lua
@@ -2,47 +2,34 @@ local M = {}
 
 function M.configure()
     local nvimTree = require("nvim-tree")
+
+    vim.g.nvim_tree_group_empty = 1
+    vim.g.nvim_tree_create_in_closed_folder = 1
+    vim.g.nvim_tree_icons = {
+        git = {
+            unstaged  = "",
+            staged    = "",
+            unmerged  = "",
+            renamed   = "➜",
+            untracked = "",
+            deleted   = "",
+            ignored   = ""
+        },
+    }
+
     nvimTree.setup({
         update_cwd = true,
         disable_netrw = true,
         hijack_netrw = true,
-        open_on_tab = true,
         hijack_unnamed_buffer_when_opening = true,
         diagnostics = { enable = true },
         update_focused_file = { enable = true },
         system_open = { cmd = "open" },
         filters = { custom = { "^\\.git$", "^\\.DS_Store$" } },
         git = { ignore = false },
-        view = {
-            width = 30,
-            height = 30,
-            side = "left",
-            preserve_window_proportions = false,
-            number = false,
-            relativenumber = false,
-            signcolumn = "yes",
-        },
         renderer = {
             icons = {
                 git_placement = "after",
-            },
-        },
-        actions = {
-            change_dir = {
-                enable = true,
-                global = false,
-            },
-            open_file = {
-                quit_on_open = false,
-                resize_window = true,
-                window_picker = {
-                    enable = true,
-                    chars = "hjkl1234567890",
-                    exclude = {
-                        filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-                        buftype = { "nofile", "terminal", "help" },
-                    },
-                },
             },
         },
     })
