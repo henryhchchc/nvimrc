47980cb138ccd31c4820793354b9fe755af1f67c
Henry Chu <henrytech@outlook.com>
2022-05-19 22:13:42 +0800

    Update project local config

--- .nvim/lsp.lua
+++ .nvim/lsp.lua
@@ -1,16 +1,9 @@
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
 return {
     sumneko_lua = {
         settings = {
             Lua = {
                 runtime = {
-                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                     version = 'LuaJIT',
-                    -- Setup your lua path
-                    path = runtime_path,
                 },
                 diagnostics = {
                     -- Get the language server to recognize the `vim` global
