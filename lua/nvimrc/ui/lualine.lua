8c3cd62806d456df33c4d0989c1dc43db86667de
Henry Chu <henrytech@outlook.com>
2023-07-15 09:35:51 +0800

    feat(status): Added copilot status

--- lua/nvimrc/ui/lualine.lua
+++ lua/nvimrc/ui/lualine.lua
@@ -20,104 +20,129 @@ local function configNavic()
     })
 end
 
-local function configure()
-    configNavic()
-
-    local function diff_source()
-        local gitsigns = vim.b.gitsigns_status_dict
-        if gitsigns then
-            return {
-                added = gitsigns.added,
-                modified = gitsigns.changed,
-                removed = gitsigns.removed,
-            }
-        else
-            return {}
-        end
+local function diff_source()
+    -- This is defined by gitsigns
+    ---@diagnostic disable-next-line: undefined-field
+    local gitsigns = vim.b.gitsigns_status_dict
+    if gitsigns then
+        return {
+            added = gitsigns.added,
+            modified = gitsigns.changed,
+            removed = gitsigns.removed,
+        }
+    else
+        return {}
     end
+end
 
-    local function lspName()
-        local clients = vim.lsp.get_active_clients({ bufnr = vim.api.nvim_get_current_buf() })
-        clients = vim.tbl_filter(function(it) return it.name ~= "null-ls" end, clients)
-        local lspCount = vim.tbl_count(clients)
-        local prefix = " "
-        local content = "N/A"
-        if lspCount == 0 then
-            content = "N/A"
-        elseif lspCount == 1 then
-            content = clients[1].name
-        else
-            content = string.format("%s (%d more)", clients[1].name, lspCount - 1)
-        end
-        return prefix .. content
+local function lspName()
+    local clients = vim.lsp.get_active_clients({ bufnr = vim.api.nvim_get_current_buf() })
+    clients = vim.tbl_filter(function(it) return it.name ~= "null-ls" and it.name ~= "copilot" end, clients)
+    local lspCount = vim.tbl_count(clients)
+    local prefix = " "
+    local content = "N/A"
+    if lspCount == 0 then
+        content = "N/A"
+    elseif lspCount == 1 then
+        content = clients[1].name
+    else
+        content = string.format("%s (%d more)", clients[1].name, lspCount - 1)
     end
+    return prefix .. content
+end
 
-    require("lualine").setup({
-        options = {
-            theme = "auto",
-            component_separators = { left = "", right = "" },
-            section_separators = { left = "", right = "" },
-            globalstatus = true,
+local lualineOptions = {
+    options = {
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        globalstatus = true,
+    },
+    sections = {
+        lualine_a = {
+            { "mode", icons_enabled = true },
         },
-        sections = {
-            lualine_a = {
-                { "mode", icons_enabled = true },
-            },
-            lualine_b = {
-                "branch",
-                { "diff", source = diff_source },
-                { "diagnostics", sources = { "nvim_diagnostic" } },
-            },
-            lualine_c = {
-                {
-                    "filename",
-                    file_status = true,
-                    path = 0,
-                    shorting_target = 40,
-                    symbols = {
-                        modified = "•",
-                        readonly = " [Read Only]",
-                        unnamed = " [No Name]",
-                        new = " [New File]",
-                    },
+        lualine_b = {
+            "branch",
+            { "diff", source = diff_source },
+            { "diagnostics", sources = { "nvim_diagnostic" } },
+        },
+        lualine_c = {
+            {
+                "filename",
+                file_status = true,
+                path = 0,
+                shorting_target = 40,
+                symbols = {
+                    modified = "•",
+                    readonly = " [Read Only]",
+                    unnamed = " [No Name]",
+                    new = " [New File]",
                 },
-                { "navic" },
             },
-            lualine_x = { "searchcount", "location" },
-            lualine_y = {
-                "encoding",
-                {
-                    "fileformat",
-                    icons_enabled = true,
-                    symbols = { unix = "LF", dos = "CRLF", mac = "CR" },
-                },
-                "filesize",
-                "filetype",
+            { "navic" },
+        },
+        lualine_x = { "searchcount", "location" },
+        lualine_y = {
+            "encoding",
+            {
+                "fileformat",
+                icons_enabled = true,
+                symbols = { unix = "LF", dos = "CRLF", mac = "CR" },
             },
-            lualine_z = {
-                {
-                    lspName,
-                    on_click = function() vim.cmd.LspInfo() end,
-                },
+            "filesize",
+            "filetype",
+        },
+        lualine_z = {
+            {
+                lspName,
+                on_click = function() vim.cmd.LspInfo() end,
+            },
+            {
+                function() return require("copilot_status").status_string() end,
+                cnd = function() return require("copilot_status").enabled() end,
             },
         },
-        extensions = {
-            "man",
-            "nvim-dap-ui",
-            "neo-tree",
-            "quickfix",
-            "aerial",
-            "lazy",
-            "trouble",
-            "fugitive",
+    },
+    extensions = {
+        "man",
+        "nvim-dap-ui",
+        "neo-tree",
+        "quickfix",
+        "aerial",
+        "lazy",
+        "trouble",
+        "fugitive",
+    },
+}
+
+local function configCopilotStatus()
+    require("copilot_status").setup({
+        icons = {
+            idle = " ",
+            error = " ",
+            offline = " ",
+            warning = " ",
+            loading = " ",
         },
     })
 end
 
 M.pluginSpec = {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "SmiteshP/nvim-navic" },
-    config = configure,
+    dependencies = {
+        {
+            "SmiteshP/nvim-navic",
+            config = configNavic,
+        },
+        {
+            "jonahgoldwastaken/copilot-status.nvim",
+            dependencies = { "zbirenbaum/copilot.lua" },
+            event = "BufReadPost",
+            config = configCopilotStatus,
+        },
+    },
+    opts = lualineOptions,
     event = "VeryLazy",
 }
 
