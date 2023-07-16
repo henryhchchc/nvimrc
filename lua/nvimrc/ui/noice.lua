b00faeb404907e85c670c64a1ad7fa24e5dc7ceb
Henry Chu <henrytech@outlook.com>
2023-07-16 22:56:56 +0800

    refactor(ui): Refactor theme and noice setup

--- lua/nvimrc/ui/noice.lua
+++ lua/nvimrc/ui/noice.lua
@@ -1,43 +1,46 @@
 local M = {}
 
+
+--- @type NoiceConfig
+local options = {
+    lsp = {
+        override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+        },
+    },
+    presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+    },
+    routes = {
+        {
+            filter = {
+                event = "msg_show",
+                any = {
+                    { find = "%d+L, %d+B" },
+                    { find = "; after #%d+" },
+                    { find = "; before #%d+" },
+                },
+            },
+            view = "mini",
+        },
+    },
+    views = {
+        vsplit = {
+            size = "40%",
+            enter = true,
+        },
+    },
+}
+
 --- @type LazyPluginSpec
 M.pluginSpec = {
     "folke/noice.nvim",
     event = "VeryLazy",
-    --- @type NoiceConfig
-    opts = {
-        lsp = {
-            override = {
-                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                ["vim.lsp.util.stylize_markdown"] = true,
-                ["cmp.entry.get_documentation"] = true,
-            },
-        },
-        presets = {
-            bottom_search = true,
-            command_palette = true,
-            long_message_to_split = true,
-        },
-        routes = {
-            {
-                filter = {
-                    event = "msg_show",
-                    any = {
-                        { find = "%d+L, %d+B" },
-                        { find = "; after #%d+" },
-                        { find = "; before #%d+" },
-                    },
-                },
-                view = "mini",
-            },
-        },
-        views = {
-            vsplit = {
-                size = "40%",
-                enter = true,
-            },
-        },
-    },
+    opts = options,
     dependencies = {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
