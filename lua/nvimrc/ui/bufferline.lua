7f643fcebafffe0423b47e77799e61b25de68acb
Henry Chu <henrytech@outlook.com>
2023-07-16 16:06:21 +0800

    fix(theme): bufferline highlight

--- lua/nvimrc/ui/bufferline.lua
+++ lua/nvimrc/ui/bufferline.lua
@@ -1,37 +1,40 @@
 local M = {}
 
-
---- @type bufferline.UserConfig
-local bufferlineOpts = {
-    options = {
-        diagnostics = "nvim_lsp",
-        always_show_bufferline = false,
-        close_command = function(n) require("mini.bufremove").delete(n, false) end,
-        right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
-        offsets = {
-            {
-                filetype = "neo-tree",
-                text = "Neo-tree",
-                highlight = "Title",
-                text_align = "left",
+local function configure()
+    --- @type bufferline.UserConfig
+    local bufferlineOpts = {
+        options = {
+            highlights = require("catppuccin.groups.integrations.bufferline").get(),
+            diagnostics = "nvim_lsp",
+            always_show_bufferline = false,
+            close_command = function(n) require("mini.bufremove").delete(n, false) end,
+            right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+            offsets = {
+                {
+                    filetype = "neo-tree",
+                    text = "Neo-tree",
+                    highlight = "Title",
+                    text_align = "left",
+                },
             },
         },
     }
-}
+    require("bufferline").setup(bufferlineOpts)
+end
 
 --- @type LazyPluginSpec
 M.pluginSpec = {
     "akinsho/bufferline.nvim",
     dependencies = {
-        { "echasnovski/mini.bufremove" }
+        { "echasnovski/mini.bufremove" },
+        { "catppuccin/nvim" },
     },
-    opts = bufferlineOpts,
+    config = configure,
     event = "VeryLazy",
     keys = {
         { "<leader>bp", function() vim.cmd.BufferLineTogglePin() end, desc = "Toggle pin" },
         { "<leader>bP", function() vim.cmd.BufferLineGroupClose("ungrouped") end, desc = "Delete non-pinned buffers" },
-    }
+    },
 }
-
 
 return M
