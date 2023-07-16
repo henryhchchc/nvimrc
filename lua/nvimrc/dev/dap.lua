f07df5658626b619aaf3e424d9b32e5f1b133255
Henry Chu <henrytech@outlook.com>
2023-07-16 10:47:20 +0800

    feat(dap): Borrow config from lazyvim

--- lua/nvimrc/dev/dap.lua
+++ lua/nvimrc/dev/dap.lua
@@ -1,32 +1,51 @@
 local M = {}
 
+
+--- @type LazyKeys[]
+local dapKepmaps = {
+    {
+        "<leader>dB",
+        function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end,
+        desc = "Breakpoint Condition",
+    },
+    { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+    { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+    { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+    { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
+    { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+    { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+    { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+    { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+    { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+    { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+    { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+    { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+    { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+    { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+    { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+}
+
 local function configure()
-    local dapModule = require("dap")
-    local dapuiModule = require("dapui")
-    dapuiModule.setup()
-    dapModule.listeners.after.event_initialized["dapui_config"] = function() dapuiModule.open({}) end
-    dapModule.listeners.before.event_terminated["dapui_config"] = function()
-        dapuiModule.close({})
-        dapModule.repl.close()
-    end
-    dapModule.listeners.before.event_exited["dapui_config"] = function()
-        dapuiModule.close({})
-        dapModule.repl.close()
-    end
-    local dap_virtual_text = require("nvim-dap-virtual-text")
-    dap_virtual_text.setup({})
-    dapModule.adapters.lldb = {
+    local dap = require("dap")
+    local dapui = require("dapui")
+    dapui.setup()
+
+    dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open({}) end
+    dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close({}) end
+    dap.listeners.before.event_exited["dapui_config"] = function() dapui.close({}) end
+
+    dap.adapters.lldb = {
         type = "executable",
         command = "lldb-vscode",
         name = "lldb",
     }
-    dapModule.adapters.rt_lldb = {
+    dap.adapters.rt_lldb = {
         type = "executable",
         command = "lldb-vscode",
         name = "rt_lldb",
     }
 
-    dapModule.configurations.scala = {
+    dap.configurations.scala = {
         {
             type = "scala",
             request = "launch",
@@ -44,43 +63,24 @@ local function configure()
             },
         },
     }
-    vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-            local bufnr = args.buf
-            if args.data then
-                local client = vim.lsp.get_client_by_id(args.data.client_id)
-                local dap = require("dap")
-                local dapui = require("dapui")
-                local telescope = require("telescope")
-                telescope.load_extension("dap")
-                vim.keymap.set("n", "<leader>td", dapui.toggle, { buffer = bufnr, desc = "DAP Toggle UI" })
-                vim.keymap.set("n", "<leader>du", dapui.toggle, { buffer = bufnr, desc = "DAP Toggle UI" })
-                vim.keymap.set(
-                    "n",
-                    "<leader>db",
-                    dap.toggle_breakpoint,
-                    { buffer = bufnr, desc = "DAP Toggle Breakpoint" }
-                )
-                vim.keymap.set("n", "<leader>dc", dap.continue, { buffer = bufnr, desc = "DAP Continue" })
-                vim.keymap.set("n", "<leader>dsi", dap.step_into, { buffer = bufnr, desc = "DAP Step Into" })
-                vim.keymap.set("n", "<leader>dso", dap.step_over, { buffer = bufnr, desc = "DAP Step Over" })
-                vim.keymap.set("n", "<leader>dst", dap.step_out, { buffer = bufnr, desc = "DAP Step Out" })
-                vim.keymap.set(
-                    "n",
-                    "<leader>dt",
-                    telescope.extensions.dap.commands,
-                    { desc = "Telescope DAP Commands" }
-                )
-            end
-        end,
-    })
 end
+
+
+--- @type LazyPluginSpec
+local dapUi = {
+    "rcarriga/nvim-dap-ui",
+    keys = {
+        { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
+        { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = { "n", "v" } },
+    },
+}
 
 --- @type LazyPluginSpec
 M.pluginSpec = {
     "mfussenegger/nvim-dap",
     config = configure,
-    dependencies = { { "rcarriga/nvim-dap-ui" }, { "theHamsta/nvim-dap-virtual-text" } },
+    dependencies = { dapUi, { "theHamsta/nvim-dap-virtual-text", config = true }, },
+    keys = dapKepmaps,
     event = "LspAttach",
 }
 
