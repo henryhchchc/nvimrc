0ee3f039802cb06a21f25cb9d4f2952073ff43a8
Henry Chu <henrytech@outlook.com>
2023-07-17 22:47:49 +0800

    feat(options): Sensible options

--- lua/nvimrc/global.lua
+++ lua/nvimrc/global.lua
@@ -1,21 +1,29 @@
-vim.opt.number = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.cursorline = true
-vim.opt.title = true
+vim.opt.autowrite = true
 vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
-vim.opt.timeoutlen = 300
-vim.opt.updatetime = 100
-vim.opt.splitright = true
-vim.opt.spelllang = { "en_us", "cjk" }
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.grepformat = "%f:%l:%c:%m"
+vim.opt.grepprg = "rg --vimgrep"
 vim.opt.ignorecase = true
-vim.opt.smartcase = true
 vim.opt.list = true
 vim.opt.listchars:append("tab:> ,trail:⋅,nbsp:⋅,multispace:⋅,conceal:?")
-vim.opt.grepprg = "rg --vimgrep"
-
-vim.undofile = true
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.shortmess:append({ C = true })
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.spelllang = { "en_us", "cjk" }
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.splitkeep = "screen"
+vim.opt.tabstop = 4
+vim.opt.timeoutlen = 300
+vim.opt.title = true
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 100
+vim.opt.wildmode = "longest:full,full"
+vim.opt.wrap = false
 
 vim.g.mapleader = " "
 vim.g.tex_flavor = "latex"
