6ba06e52946bfdeebe7cc7a0d3fd505da4af9300
Henry Chu <henrytech@outlook.com>
2023-07-17 21:02:22 +0800

    fix(cmp): Fixed tab keymap

--- lua/nvimrc/dev/cmp/cmpkeymap.lua
+++ lua/nvimrc/dev/cmp/cmpkeymap.lua
@@ -1,28 +1,14 @@
-local has_words_before = function()
-    local loc = vim.api.nvim_win_get_cursor(0)
-    local line, col = loc[1], loc[2]
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local tabMapping = function(fallback)
-    local cmp = require("cmp")
     local luasnip = require("luasnip")
-    if cmp.visible() then
-        cmp.select_next_item()
-    elseif luasnip.expand_or_jumpable() then
+    if luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
-    elseif has_words_before() then
-        cmp.complete()
     else
         fallback()
     end
 end
 local sTabMapping = function(fallback)
-    local cmp = require("cmp")
     local luasnip = require("luasnip")
-    if cmp.visible() then
-        cmp.select_prev_item()
-    elseif luasnip.jumpable(-1) then
+    if luasnip.jumpable(-1) then
         luasnip.jump(-1)
     else
         fallback()
