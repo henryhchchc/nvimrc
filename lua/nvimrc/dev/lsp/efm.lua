8c229575335a2e734ae39195d30251ef6b9aea85
Henry Chu <henrytech@outlook.com>
2023-07-25 10:03:48 +0800

    feat(efm): added more options

--- lua/nvimrc/dev/lsp/efm.lua
+++ lua/nvimrc/dev/lsp/efm.lua
@@ -1,8 +1,27 @@
 local M = {}
 
+local efm_filetypes = {
+  "css",
+  "html",
+  "javascript",
+  "typescript",
+  "typescript",
+  "graphql",
+  "python",
+  "yaml",
+  "json"
+}
+
 function M.configure(defaultOpts)
   local additionalOpts = {
-    filetypes = { "python" },
+    init_options = {
+      documentRangeFormatting = true,
+      documentFormatting = true,
+      hover = true,
+      codeAction = true,
+      completion = true
+    },
+    filetypes = efm_filetypes,
   }
   local options = vim.tbl_deep_extend("force", defaultOpts, additionalOpts)
   require("lspconfig").efm.setup(options)
