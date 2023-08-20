4241f35b227323ba2da8ae86b6f5d37ec805868c
Henry Chu <henrytech@outlook.com>
2023-08-20 22:45:14 +0800

    feat(options): optimize options

--- lua/nvimrc/options.lua
+++ lua/nvimrc/options.lua
@@ -1,22 +1,27 @@
 vim.opt.autowrite = true
-vim.opt.clipboard = { "unnamed", "unnamedplus" }
+vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
 vim.opt.expandtab = true
 vim.opt.grepformat = "%f:%l:%c:%m"
 vim.opt.grepprg = "rg --vimgrep"
 vim.opt.ignorecase = true
+vim.opt.inccommand = "nosplit"
+vim.opt.laststatus = 0
 vim.opt.list = true
 vim.opt.listchars:append("tab:> ,trail:⋅,nbsp:⋅,multispace:⋅,conceal:?")
+vim.opt.mouse = "a"
 vim.opt.number = true
-vim.opt.shiftwidth = 4
-vim.opt.shortmess:append({ C = true })
+vim.opt.shiftwidth = 2
+vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
+vim.opt.showmode = false
+vim.opt.signcolumn = "yes"
 vim.opt.smartcase = true
 vim.opt.smartindent = true
 vim.opt.spelllang = { "en_us", "cjk" }
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.splitkeep = "screen"
-vim.opt.tabstop = 4
+vim.opt.tabstop = 2
 vim.opt.timeoutlen = 300
 vim.opt.title = true
 vim.opt.titlestring = "%t - NVIM"
@@ -31,7 +36,7 @@ vim.g.tex_flavor = "latex"
 
 vim.opt.termguicolors = true
 
-vim.opt.conceallevel = 2
+vim.opt.conceallevel = 3
 vim.opt.concealcursor = "nc"
 
 -- Disable netrw
