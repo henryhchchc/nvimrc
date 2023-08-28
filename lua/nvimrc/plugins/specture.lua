9ff8ea8a66845b33e1b9a1dbfcb4f8ae75bdfb17
Henry Chu <henrytech@outlook.com>
2023-08-28 14:17:54 +0800

    feat(editing): add specture

---
+++ lua/nvimrc/plugins/specture.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-pack/nvim-spectre",
+  cmd = "Spectre",
+  opts = { open_cmd = "noswapfile vnew" },
+  -- stylua: ignore
+  keys = {
+    { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
+  },
+}
