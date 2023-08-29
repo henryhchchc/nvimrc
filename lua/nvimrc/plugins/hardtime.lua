38578078cdf7dc1ff3fa1bf4a415585c00d6b435
Henry Chu <henrytech@outlook.com>
2023-08-29 20:58:38 +0800

    feat(keymap): add harttime

---
+++ lua/nvimrc/plugins/hardtime.lua
@@ -0,0 +1,20 @@
+--- @type LazyPluginSpec
+return {
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  event = "VeryLazy",
+  opts = {
+    disable_mouse = false,
+    disabled_filetypes = {
+      "help",
+      "neo-tree",
+      "NeogitStatus",
+      "terminal",
+      "Trouble",
+      "qf",
+      "Outline",
+      "neotest-summary",
+      "neotest-output-panel",
+    }
+  },
+}
