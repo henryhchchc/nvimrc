15f22d6e02a18a0b3dba7fc086955a474a279060
Henry Chu <henrytech@outlook.com>
2023-08-30 13:17:07 +0800

    fix(keymap): hop

--- lua/nvimrc/plugins/hop.lua
+++ lua/nvimrc/plugins/hop.lua
@@ -4,37 +4,49 @@ return {
   --- @param self LazyPlugin
   --- @param keys string[]
   --- @return LazyKeys[]
-  keys = function(self, keys)
-    local hop = require("hop")
-    local dir = require("hop.hint").HintDirection
-    --- @type LazyKeys[]
-    return {
-      {
-        "f",
-        function() hop.hint_char1({ direction = dir.AFTER_CURSOR, current_line_only = true }) end,
-        remap = true,
-        mode = { "n", "v", "o" },
-      },
-      {
-        "F",
-        function() hop.hint_char1({ direction = dir.BEFORE_CURSOR, current_line_only = true }) end,
-        remap = true,
-        mode = { "n", "v", "o" },
-      },
-      {
-        "t",
-        function() hop.hint_char1({ direction = dir.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end,
-        remap = true,
-        mode = { "n", "v", "o" },
-      },
-      {
-        "T",
-        function() hop.hint_char1({ direction = dir.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end,
-        remap = true,
-        mode = { "n", "v", "o" },
-      },
-    }
-  end,
+  keys = {
+    {
+      "f",
+      function()
+        local hop = require("hop")
+        local dir = require("hop.hint").HintDirection
+        hop.hint_char1({ direction = dir.AFTER_CURSOR, current_line_only = true })
+      end,
+      remap = true,
+      mode = { "n", "v", "o" },
+    },
+    {
+      "F",
+      function()
+        local hop = require("hop")
+        local dir = require("hop.hint").HintDirection
+        hop.hint_char1({ direction = dir.BEFORE_CURSOR, current_line_only = true })
+      end,
+      remap = true,
+      mode = { "n", "v", "o" },
+    },
+    {
+      "t",
+      function()
+        local hop = require("hop")
+        local dir = require("hop.hint").HintDirection
+        hop.hint_char1({ direction = dir.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+      end,
+      remap = true,
+      mode = { "n", "v", "o" },
+    },
+    {
+      "T",
+      function()
+        local hop = require("hop")
+        local dir = require("hop.hint").HintDirection
+        hop.hint_char1({ direction = dir.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+      end,
+      remap = true,
+      mode = { "n", "v", "o" },
+    },
+  }
+  ,
   config = true,
   event = "VeryLazy"
 }
