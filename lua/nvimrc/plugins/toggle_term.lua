c71c00931e4c65c0f89268e3ae59b92ef0a7ddc1
Henry Chu <henrytech@outlook.com>
2023-09-15 14:01:09 +0800

    feat(term): update toggle term shortcuts

--- lua/nvimrc/plugins/toggle_term.lua
+++ lua/nvimrc/plugins/toggle_term.lua
@@ -8,9 +8,20 @@ return {
     "TermSelect",
   },
   opts = {
-
+    size = function(term)
+      if term.direction == "horizontal" then
+        return 25
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
   },
   keys = {
-    { "<leader>ut", function() vim.cmd.ToggleTerm() end, desc = "Toggle terminal" }
+    { "<leader><cr>", function() vim.cmd.ToggleTerm() end, desc = "Toggle terminal" },
+    {
+      "<leader><A-cr>",
+      function() vim.cmd.ToggleTerm({ args = { "direction=float" } }) end,
+      desc = "Toggle terminal",
+    },
   }
 }
