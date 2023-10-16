51cf88639a75d5c7ed685655f8401290fb10440e
Henry Chu <henrytech@outlook.com>
2023-10-16 22:09:33 +0800

    feat(cmp): update keymap

--- lua/nvimrc/plugins/cmp/keymap.lua
+++ lua/nvimrc/plugins/cmp/keymap.lua
@@ -4,8 +4,8 @@ local tab_mapping = function(fallback)
   local copilot_suggestion = require("copilot.suggestion")
   if copilot_suggestion.is_visible() then
     copilot_suggestion.accept()
-  elseif luasnip.expand_or_jumpable() then
-    luasnip.expand_or_jump()
+  elseif luasnip.expandable() then
+    luasnip.expand()
   elseif cmp.visible() and cmp.get_active_entry() then
     cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false })
   else
@@ -13,9 +13,17 @@ local tab_mapping = function(fallback)
   end
 end
 
+
 local tap_mapping_cmdline = function(fallback)
   local cmp = require("cmp")
   if cmp.visible() then cmp.select_next_item() end
+end
+
+local snip_jump = function(fallback)
+  local luasnip = require("luasnip")
+  if luasnip.jumpable(1) then
+    luasnip.jump(1)
+  end
 end
 
 local shift_tab_mapping = function(fallback)
@@ -56,6 +64,7 @@ M.insert = {
   ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
   ["<C-e>"] = cmp.mapping(cmp.mapping.close(), { "i", "c" }),
   ["<TAB>"] = cmp.mapping(tab_mapping, { "i", "c", "s" }),
+  ["<C-;>"] = cmp.mapping(snip_jump, { "i", "c", "s" }),
   ["<S-TAB>"] = cmp.mapping(shift_tab_mapping, { "i", "c", "s" }),
   ["<CR>"] = cmp.mapping(return_mapping, { "i" }),
   ["<C-s>"] = cmp.mapping(copilot_suggest, { "i" }),
