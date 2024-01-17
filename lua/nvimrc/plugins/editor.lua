2412bf735c1249d2b622761b9c5fec86445b930a
Henry Chu <henrytech@outlook.com>
2024-01-17 21:39:03 +0800

    fix(editor): remove unused plugins

--- lua/nvimrc/plugins/editor.lua
+++ lua/nvimrc/plugins/editor.lua
@@ -6,7 +6,6 @@ return {
     opts = { check_ts = true },
     event = "InsertEnter",
   },
-  { "RRethy/nvim-align", cmd = "Align" },
   { "numToStr/Comment.nvim", config = true, event = "VeryLazy" },
   { "kylechui/nvim-surround", config = true, event = { "BufReadPost", "BufNewFile" } },
   {
@@ -17,11 +16,5 @@ return {
       })
     end,
     event = { "BufReadPost", "BufNewFile" },
-  },
-  {
-    "karb94/neoscroll.nvim",
-    enabled = false,
-    event = "VeryLazy",
-    opts = { easing_function = "cubic" },
   },
 }
