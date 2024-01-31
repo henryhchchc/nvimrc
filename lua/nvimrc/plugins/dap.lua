5446d339a934ec656338256b064af0a6640cbf62
Henry Chu <henrytech@outlook.com>
2024-01-31 12:49:53 +0800

    refactor(dap): config

--- lua/nvimrc/plugins/dap.lua
+++ lua/nvimrc/plugins/dap.lua
@@ -25,15 +25,20 @@ local keymaps = {
 local function configure()
   local dap = require("dap")
   local dapui = require("dapui")
-  dapui.setup()
+  --- @type dapui.Config
+  local dapui_optons = {
+
+  }
+  dapui.setup(dapui_optons)
 
   dap.listeners.after.event_initialized["dapui_config"] = function () dapui.open({}) end
   dap.listeners.before.event_terminated["dapui_config"] = function () dapui.close({}) end
   dap.listeners.before.event_exited["dapui_config"] = function () dapui.close({}) end
 
+
   dap.adapters.lldb = {
     type = "executable",
-    command = "lldb-vscode",
+    command = "lldb-dap",
     name = "lldb",
   }
 end
@@ -43,13 +48,16 @@ return {
   {
     "mfussenegger/nvim-dap",
     config = configure,
-    dependencies = {},
+    dependencies = {
+      { "rcarriga/nvim-dap-ui" },
+      { "theHamsta/nvim-dap-virtual-text", config = true },
+    },
     keys = keymaps,
     event = "LspAttach",
   },
-  { "theHamsta/nvim-dap-virtual-text", config = true },
   {
     "rcarriga/nvim-dap-ui",
+    event = "LspAttach",
     keys = {
       { "<leader>du", function () require("dapui").toggle({}) end, desc = "Dap UI" },
       { "<leader>de", function () require("dapui").eval() end, desc = "Eval", mode = { "n", "v" } },
