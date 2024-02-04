54047fbd2d4d3496749bd6f7906d459dc3914585
Henry Chu <henrytech@outlook.com>
2024-02-04 16:52:34 +0800

    fix(autocmd): disable buggy restore cursor

--- lua/nvimrc/config/autocmds.lua
+++ lua/nvimrc/config/autocmds.lua
@@ -91,13 +91,3 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end
   end,
 })
-
-vim.api.nvim_create_autocmd("BufReadPost", {
-  group = vim.api.nvim_create_augroup("restore_cursor", {}),
-  callback = function ()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
-      vim.api.nvim_win_set_cursor(0, mark)
-    end
-  end,
-})
