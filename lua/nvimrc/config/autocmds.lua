06864bf2e6a6b000151e2b66e9350ea5fbc7b090
Henry Chu <henrytech@outlook.com>
2024-05-10 11:09:44 +0800

    feat(autocmd): restore cursor

--- lua/nvimrc/config/autocmds.lua
+++ lua/nvimrc/config/autocmds.lua
@@ -92,3 +92,21 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end
   end,
 })
+
+-- Go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("last_loc", {}),
+  callback = function (event)
+    local exclude = { "gitcommit" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) then
+      return
+    end
+    vim.b[buf].lazyvim_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
