10c3be7b266a5fdfa745bd9aa9b9b301f8ca9167
Henry Chu <henrytech@outlook.com>
2024-05-20 10:01:18 +0800

    feat(editor): remove Comment.nvim
    in favor of native comment keymaps

--- lua/nvimrc/plugins/editor.lua
+++ lua/nvimrc/plugins/editor.lua
@@ -6,7 +6,6 @@ return {
     opts = { check_ts = true },
     event = "InsertEnter",
   },
-  { "numToStr/Comment.nvim", config = true, event = "VeryLazy" },
   { "kylechui/nvim-surround", config = true, event = { "BufReadPost", "BufNewFile" } },
   {
     "RRethy/vim-illuminate",
