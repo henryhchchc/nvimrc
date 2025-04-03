8f0240f63381e155acd869f243c340b9b421b913
Henry Chu <henrytech@outlook.com>
2025-04-03 09:52:33 +0800

    Update clangd config

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,6 +1,6 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { "clangd", "--background-index", "--offset-encoding=utf-8" },
-  root_markers = { ".git/", "compile_commands.json", ".clangd" },
+  cmd = { "clangd", "--offset-encoding=utf-8" },
+  root_markers = { ".git/", ".clangd" },
   filetypes = { "c", "cpp" },
 }
