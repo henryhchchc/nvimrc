b842f340c702eaacdbb0ea38a5a4522973a12fda
Henry Chu <henrytech@outlook.com>
2025-10-26 11:43:48 +0800

    Add message for lazy.nvim install

--- lua/nvimrc/lazy.lua
+++ lua/nvimrc/lazy.lua
@@ -1,6 +1,9 @@
 local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazy_path) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.api.nvim_echo({
+    { "Downloading and installing lazy.nvim" },
+  }, false, {})
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazy_path })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
