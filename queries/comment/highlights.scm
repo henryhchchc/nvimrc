ef440dac040f8b270c4f94c093d808c61655d4cc
Henry Chu <henrytech@outlook.com>
2025-12-29 14:59:57 +0800

    Highlight SAFETY for Rust

---
+++ queries/comment/highlights.scm
@@ -0,0 +1,8 @@
+((tag
+  (name) @comment.warning @nospell
+  ("(" @punctuation.bracket
+    (user) @constant
+    ")" @punctuation.bracket)?
+  ":" @punctuation.delimiter)
+  (#any-of? @comment.warning "SAFETY"))
+
